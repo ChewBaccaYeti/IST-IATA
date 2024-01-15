c108238a3af02f121138348bec8fecaecdce6c81
Venedykt Mitishov <chewbaccayeti@gmail.com>
2024-01-15 21:24:37 +0200

    Flights is completed but I still don't know why I have duplicates

--- IST.js
+++ IST.js
@@ -167,8 +167,8 @@ function dataFlights() {
                                                     };
                                                     const cs_fields = {
                                                         "cs_airline_iata": null,
-                                                        "cs_flight_number": null,
                                                         "cs_flight_iata": null,
+                                                        "cs_flight_number": null,
                                                     };
                                                     const arr_fields = {
                                                         "arr_baggage": flight.carousel || null,
@@ -210,45 +210,26 @@ function dataFlights() {
                                                         ...arr_fields,
                                                         ...dep_fields,
                                                     };
-                                                    const cloned_flight = flight.codeshare.map((code) => {
-                                                        const cloned_fields = {
-                                                        "cs_airline_iata": original_flight.airline_iata || null,
-                                                        "cs_flight_number": original_flight.flight_number || null,
-                                                        "cs_flight_iata": original_flight.flight_iata || null,
+                                                    const flights_bucket = {
+                                                        ...original_flight,
+                                                    };
+                                                
+                                                    // Фильтруем по статусу и добавляем родителя в массив
+                                                    if (status === 1 ? flight : status === 0 ? flight : null) {
+                                                        newFlightsArray.push(flights_bucket);
+                                                        // Добавляем детей в массив
+                                                    newFlightsArray.push(...flight.codeshare.map((code) => ({
+                                                        ...flights_bucket,
+                                                        "cs_airline_iata": flights_bucket.airline_iata || null,
+                                                        "cs_flight_number": flights_bucket.flight_number || null,
+                                                        "cs_flight_iata": flights_bucket.flight_iata || null,
                                                         "airline_iata": status === 0 ? code.slice(0, 2) : status === 1 ? code.slice(0, 2) : null,
                                                         "flight_iata": status === 0 ? code : status === 1 ? code : null,
                                                         "flight_number": status === 0 ? code.slice(2, 6) : status === 1 ? code.slice(2, 6) : null,
-                                                        };
-                                                        if (
-                                                            type === 1 ? (status === 1 ? flight && code : status === 0 ? flight && code : null) : 
-                                                            type === 0 ? (status === 0 ? flight && code : status === 1 ? flight && code : null) : null
-                                                            ) return cloned_fields;
-                                                    });
-
-                                                    const flights_bucket = {
-                                                        ...original_flight,
-                                                        ...cloned_flight[0], // Поскольку map возвращает массив, возьмем первый элемент
-                                                    };
-                                                    // Фильтрую по типу и при совпадении фильтр проходит глубже по статусу
-                                                    if (status === 1 ? flight : status === 0 ? flight : null) {
+                                                    })));
                                                         return flights_bucket;
-                                                    }
-                                                    // Создаю новый set для уникальных рейсов
-                                                    const uniqueFlights = [...new Set([...flights_bucket])]; 
-                                                    return uniqueFlights;
-                                                });
+                                                    }});                                            
 
-                                                for (const flight in newFlightsArray) {
-                                                    const cloned_flight = { ...newFlightsArray[flight] };
-                                                    newFlightsArray[flight] = cloned_flight;
-                                                    console.log(cloned_flight);
-                                                }
-
-                                                newFlightsArray.push(...newFlightsFields);
-                                                console.log(`Page ${pageNumber}, Date ${date}`.blue, newFlightsFields);
-                                                console.log('Request completed successfully.'.green);
-                                                console.log(`Proxy is configured and request were made via proxy: ${proxy}`.cyan);
-                                                
                                                 if (newFlightsFields.length >= max_page_size) {
                                                     finished = false;
                                                     console.log("Loop is still running...".bgYellow);
@@ -257,7 +238,10 @@ function dataFlights() {
                                                     console.log("Loop is over.".bgBlue);
                                                 }
                                                 retry_done();
-                                            
+
+                                                console.log(`Page ${pageNumber}, Date ${date}`.blue, newFlightsFields,);
+                                                console.log('Request completed successfully.'.green);
+                                                console.log(`Proxy is configured and request were made via proxy: ${proxy}`.cyan);
                                                 return newFlightsFields;
 
                                             } catch (error) {
@@ -280,6 +264,11 @@ function dataFlights() {
         }, next_date);
     });
 }
+
+// Создаю новый set для уникальных рейсов
+// const uniqueFlights = [...new Set([...flights_bucket])]; 
+// return uniqueFlights;
+
 // if (type === 1 ? (status === 1 ? flight : status === 0 ? flight : null) : 
 //     type === 0 ? (status === 0 ? flight : status === 1 ? flight : null) : null) {
 // return flights_bucket;
